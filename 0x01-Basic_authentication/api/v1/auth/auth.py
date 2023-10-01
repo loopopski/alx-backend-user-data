ab8cc21d039372ee2c9496080a4faf1c53764019
Ibukun Obideyi <49342907+loopopski@users.noreply.github.com>
2023-09-30 21:24:17 -0700

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,42 +1,56 @@
 #!/usr/bin/env python3
-"""
-Auth class
-"""
-
-from tabnanny import check
+"""This file contains the Auth class"""
+from typing import List, TypeVar
 from flask import request
-from typing import TypeVar, List
-User = TypeVar('User')
 
 
 class Auth:
-    """
-    a class to manage the API authentication
-    """
+    """The Auth Class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        returns False - path and excluded_paths
-        """
-        check = path
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """The require_auth method"""
+        if path is None:
             return True
-        if path[-1] != "/":
-            check += "/"
-        if check in excluded_paths or path in excluded_paths:
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        full_path = "{}/".format(path)
+        if path in excluded_paths or full_path in excluded_paths:
             return False
+        if path.endswith("/"):
+            abs_path = path[:-1]
+        else:
+            abs_path = path
+        # for url in excluded_paths:
+        #     if url.endswith("*/"):
+        #         extracted_path = url[:-2]
+        #         sub_path = abs_path[0: len(extracted_path)]
+        #         if sub_path == extracted_path:
+        #             return False
+        #     if url.endswith("*"):
+        #         extracted_path = url[:-1]
+        #         sub_path = abs_path[0: len(extracted_path)]
+        #         if sub_path == extracted_path:
+        #             return False
+        for url in excluded_paths:
+            extracted_path = ""
+            if url.endswith("*/"):
+                extracted_path = url[:-2]
+            if url.endswith("*"):
+                extracted_path = url[:-1]
+            if len(extracted_path) > 0:
+                sub_path = abs_path[0: len(extracted_path)]
+                if sub_path == extracted_path:
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """
-        returns None - request
-        """
+        """The authorization_header method"""
         if request is None:
             return None
-        return request.headers.get("Authorization")
+        if 'Authorization' not in request.headers.keys():
+            return None
+        return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> User:
-        """
-        returns None - request
-        """
+    def current_user(self, request=None) -> TypeVar('User'):
+        """The current_user method"""
         return None
